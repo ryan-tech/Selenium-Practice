72c8848ba0c441608ca12496638ffea187033cb8
ryan-tech <ryankim.dev@gmail.com>
2021-06-03 22:41:42 -0700

    Finished target function

--- src/main/java/WebDriverTest.java
+++ src/main/java/WebDriverTest.java
@@ -1,8 +1,11 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.*;
 import java.util.concurrent.TimeUnit;
@@ -73,8 +76,79 @@ public class WebDriverTest {
     }
 
 
+    public static void target() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Ryan\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+
+        //options.addArguments("--headless"); // uncomment this after you're done
+        options.addArguments("--start-maximized");
+
+        // create new driver
+        WebDriver targetDriver = new ChromeDriver(options);
+        // practicing explicit waits
+        WebDriverWait wait = new WebDriverWait(targetDriver, 20);
+
+        targetDriver.get("https://www.target.com");
+
+        // find search bar
+        WebElement searchBar = targetDriver.findElement(By.id("search"));
+        // type iphone in search bar
+        searchBar.sendKeys("iphone");
+
+        System.out.println("Waiting for button to appear...");
+        // click on search
+        WebElement searchButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@class=\"SearchInputButton-sc-1opoijs-0 eOzuAz\"]")));
+        searchButton.click();
+
+        System.out.println("Letting page load...");
+
+        // let the page load
+        // using implicit waits
+        targetDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS) ;
+
+        // scroll to the bottom of the page
+        JavascriptExecutor js = (JavascriptExecutor) targetDriver;
+        js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+
+        System.out.println("Finding products");
+
+        // output product description and price for all products displayed on page one to a text file
+        // class="s-item    s-item__sep-on-bottom      " is a sponsored item!!!!!
+
+        List<WebElement> products = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//li[@data-test='list-entry-product-card']")));
+        // sample output
+        System.out.println(products.size() + " products found");
+        // iterate through the list and get the name and price
+
+        // Apple iphone 7/7 plus 32GB/128GB Unlocked Verizon at&t Smartphone  - $114.00 to $357.00
+        try {
+            // open a file
+            FileWriter myWriter = new FileWriter("target.txt");
+            // write to the file
+            for(WebElement product : products) {
+                js.executeScript("arguments[0].scrollIntoView(true);",product); // scroll until the product element is in view
+                //System.out.println(product.getText());
+                wait.until(ExpectedConditions.presenceOfNestedElementLocatedBy(product, By.xpath(".//a[@data-test=\"product-title\"]")));
+                WebElement title = product.findElement(By.xpath(".//a[@data-test=\"product-title\"]"));
+
+                wait.until(ExpectedConditions.presenceOfNestedElementLocatedBy(product, By.cssSelector("div[data-test='current-price']")));
+                WebElement price = product.findElement(By.cssSelector("div[data-test='current-price']"));
+                //System.out.println(title.getText() + " " + price.getText() + "\n");
+                myWriter.write(title.getText() + " " + price.getText() + "\n");
+            }
+            // close the file
+            myWriter.close();
+            System.out.println("[target] Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("[target] An error occurred.");
+            e.printStackTrace();
+        }
+        targetDriver.quit();
+    }
+
+
     public static void main(String[] args) throws InterruptedException {
         ebay();
-
+        target();
     }
 }
